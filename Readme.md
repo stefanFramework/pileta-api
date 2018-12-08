b26888f47b303ea2b52a6a9233a42dc739b1c0f8
stefanFramework <cesar_cappetto@yahoo.com.ar>
2018-12-08 18:29:59 -0300

    Clean slate

--- Readme.md
+++ Readme.md
@@ -1,12 +1,6 @@
 # Pileta API
 
-#### Init
 ```
-API: sh bootstrap.sh build
-```
-
-#### Runing
-```
-API: sh bootstrap.sh start
-DB: docker run -d -p 5432:3306 --name mysql_db -e MYSQL_ROOT_PASSWORD=root mysql:latest
+building the image  --> sh init.sh build 
+running the app     --> sh init.sh run
 ```
---
+++ init.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+print_bold() {
+    MESSAGE=$1; shift
+    echo "\033[1;32m$MESSAGE\033[0m"
+}
+
+help_menu() {
+   print_bold "Available Commands"
+    echo ""
+    echo "\033[1;34mbuild\033[0m    Build all the images"
+    echo "run      Runs all the containers"
+    echo "\033[1;34mstop\033[0m     Stops all the containers"
+    echo "tests    Run unit test suite"
+    echo "\033[1;34mlogs\033[0m     tail -f to the application logs"
+    echo ""
+}
+
+build() {
+    print_bold "Building ..."
+    docker-compose build
+    print_bold "Building Succesfull"
+}
+
+run() {
+    docker-compose up -d
+}
+
+stop() {
+    docker-compose down
+}
+
+tests() {
+    echo "No Test suite available at the moment"
+}
+
+logs() {
+    echo "No Log files available at the moment"
+}
+
+COMMAND=$1; shift
+case $COMMAND in
+    build)
+        build
+    ;;
+    run)
+        run
+    ;;
+    stop)
+        stop
+    ;;
+    tests)
+        tests
+    ;;
+    logs)
+        logs
+    ;;
+    *)
+        help_menu
+        echo "Invalid command $COMMAND"
+        exit 1
+    ;;
+esac
