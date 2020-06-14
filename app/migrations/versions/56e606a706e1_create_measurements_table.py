488c94db5f94d9801ace5c95f64a1796bf040b3f
stefanFramework <cesar_cappetto@yahoo.com.ar>
2020-06-14 20:48:42 -0300

    Generating migrations

---
+++ app/migrations/versions/56e606a706e1_create_measurements_table.py
@@ -0,0 +1,38 @@
+"""create_measurements_table
+
+Revision ID: 56e606a706e1
+Revises: e62a2de1f140
+Create Date: 2020-06-14 23:43:33.826268
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
+
+# revision identifiers, used by Alembic.
+revision = '56e606a706e1'
+down_revision = 'e62a2de1f140'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('measurements',
+        sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
+        sa.Column('date', sa.DateTime(), nullable=False),
+        sa.Column('device_id', sa.Integer(), nullable=False),
+        sa.Column('variable_id', sa.String(length=255), nullable=False),
+        sa.Column('value', sa.Float(), nullable=False),
+        sa.Column('created_at', sa.DateTime(), nullable=False),
+        sa.Column('updated_at', sa.DateTime(), nullable=False),
+        sa.Column('deleted_at', sa.DateTime(), nullable=True),
+        sa.Column('request_payload', sa.Text(), nullable=False),
+        
+        sa.ForeignKeyConstraint(['device_id'], ['devices.id']),
+        sa.ForeignKeyConstraint(['variable_id'], ['variables.id']),
+        sa.PrimaryKeyConstraint('id')
+    )
+
+
+def downgrade():
+   op.drop_table('measurements')
---
+++ app/migrations/versions/e62a2de1f140_create_variables_table.py
@@ -0,0 +1,35 @@
+"""create_variables_table
+
+Revision ID: e62a2de1f140
+Revises: 1e41e995e7c4
+Create Date: 2020-06-14 23:22:56.712757
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
+
+# revision identifiers, used by Alembic.
+revision = 'e62a2de1f140'
+down_revision = '1e41e995e7c4'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    
+    op.create_table('variables',
+        sa.Column('id', sa.String(length=255), nullable=False),
+        sa.Column('name', sa.String(length=255), nullable=False),
+        sa.Column('min_value', sa.Float(), nullable=False),
+        sa.Column('max_value', sa.Float(), nullable=False),
+        sa.Column('device_id', sa.Integer(), nullable=False),
+        sa.Column('created_at', sa.DateTime(), nullable=False),
+        sa.Column('updated_at', sa.DateTime(), nullable=False),
+        sa.Column('deleted_at', sa.DateTime(), nullable=True),
+        sa.ForeignKeyConstraint(['device_id'], ['devices.id']),
+        sa.PrimaryKeyConstraint('id')
+    )
+
+def downgrade():
+   op.drop_table('variables')
